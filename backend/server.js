3f1ed0d0b91051631d22706b210ba83126ce025a
kawin <kawin2526jj@gmail.com>
2024-07-20 17:52:55 +0700

    edit

---
+++ backend/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sequelize = require('./config/database');
+const Todo = require('./models/todo');
+
+const app = express();
+const port = 2222;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Sync database
+sequelize.sync()
+
+// Get all todos
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.findAll();
+  res.json(todos);
+});
+
+// Create a new todo
+app.post('/todos', async (req, res) => {
+  const { topic, name, url, detail, done } = req.body;
+  const newTodo = await Todo.create({ topic, name, url, detail, done });
+  res.json(newTodo);
+});
+
+// Update a todo
+app.put('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { topic, name, url, detail, done } = req.body;
+  await Todo.update({ topic, name, url, detail, done }, { where: { id } });
+  const updatedTodo = await Todo.findByPk(id);
+  res.json(updatedTodo);
+});
+
+// Delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params;
+  await Todo.destroy({ where: { id } });
+  res.json({ message: 'Todo deleted' });
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
---
+++ db/init.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE IF NOT EXISTS card_todo_db;
